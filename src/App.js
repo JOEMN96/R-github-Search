71590a5e8f23fd3921dd7259baed2cdbfea4b0f3
Joemn <Aruljoe37@gmail.com>
2020-11-07 21:34:27 +0530

    auth0 stripped

--- src/App.js
+++ src/App.js
@@ -3,29 +3,25 @@ import NavBar from "./components/Layout/Navbar";
 import ErrorPage from "./components/Layout/ErrorPage";
 import Login from "./components/Layout/Login";
 import Home from "./components/Layout/home";
-import PrivateRoute from "./components/Layout/PrivateRoute";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import AuthWrapper from "./components/Layout/AuthWrapper";
 
 function App() {
   return (
-    <AuthWrapper>
-      <Router>
-        <Switch>
-          <PrivateRoute path="/" exact>
-            <NavBar />
-            <Home />
-          </PrivateRoute>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="*">
-            <ErrorPage />
-          </Route>
-        </Switch>
-      </Router>
-    </AuthWrapper>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <NavBar />
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "./context/context";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 import "./index.css";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={process.env.REACT_APP_DOMAIN}
-    clientId={process.env.REACT_APP_CLIENTID}
-    redirectUri={window.location.origin}
-    cacheLocation="localstorage"
-  >
-    <Provider>
-      <App />
-    </Provider>
-  </Auth0Provider>,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
